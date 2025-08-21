67c70b5dc4b304c80b1ec726db1b03c0640fea12
lucascogrossi <lucasmachado292@gmail.com>
2025-08-21 13:49:47 -0300

    rm mobile & wifi

--- start-tmux.sh
+++ start-tmux.sh
@@ -16,12 +16,5 @@ if ! tmux has-session -t pgdev 2>/dev/null; then
   tmux send-keys -t pgdev:pgdev-shell "cd unicentro/banco-de-dados-II && ./pgdev.sh" C-m
 fi
 
-if ! tmux has-session -t dev-mobile 2>/dev/null; then
-    tmux new-session -d -s dev-mobile -n zsh
-    tmux send-keys -t dev-mobile:zsh "cd unicentro/desenvolvimento-dispositivos-moveis" C-m
-    tmux new-window -t dev-mobile -n emulator
-    tmux send-keys -t dev-mobile:emulator "cd unicentro/desenvolvimento-dispositivos-moveis" C-m
-fi
-
 tmux attach -t main:zsh
 
--- wifi-unicentro.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo systemctl start NetworkManager
-sudo nmcli con up "academicosUnicentro"
-
